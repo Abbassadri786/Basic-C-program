6d46f475de3f76423c8de9744ac7f9e862b11f25
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-09-13 19:44:39 +0530

    Update Adding_1_to_each_digit.c

--- Adding_1_to_each_digit.c
+++ Adding_1_to_each_digit.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int num,d1,d2,d3,d4,x,y,result;
-    printf("Enter a 4 digit num:-");
+    int num,d1,d2,d3,d4,d5,result;
+    printf("Enter a 5 digit num:-");
     scanf("%d",&num);
 
-   d1=num/1000;
-   y=num%1000;
-   d2=y/100;
-   x=y%100;
-   d3=x/10;
-   d4=num%10;
+    d5= num%10;
+    num=num/10;
+    d4= num%10;
+    num= num/10;
+    d3= num%10;
+    num= num/10;
+    d2= num%10;
+    d1= num/10;
 
-    result=d4*1000 + d3*100 + d2*10 + d1;
-    printf("\nReverse of a digit is = %d\n",result);
+    result= (d1+1)*10000 + (d2+1)*1000 + (d3+1)*100 + (d4+1)*10 + d5+1;
+    printf("\nNew num after adding 1 is = %d\n",result);
     return 0;
 }
-
