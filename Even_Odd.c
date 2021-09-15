80a553c5bc7f520bdddeccede07b1b88895af8dc
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-09-16 01:19:13 +0530

    Create Even_Odd.c

---
+++ Even_Odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+
+    if((num/2)*2==num)
+    { printf("\nEven Number\n");}
+    else
+    {printf("\nOdd Number\n");}
+
+    return 0;
+}
