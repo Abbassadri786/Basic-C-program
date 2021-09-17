25774b72064f17e668000c54ccddbcc4781dca93
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-09-18 02:10:50 +0530

    Update Adding_1_to_each_digit.c

--- Basic Codes/Adding_1_to_each_digit.c
+++ Basic Codes/Adding_1_to_each_digit.c
@@ -13,8 +13,19 @@ int main()
     num= num/10;
     d2= num%10;
     d1= num/10;
-
-    result= (d1+1)*10000 + (d2+1)*1000 + (d3+1)*100 + (d4+1)*10 + d5+1;
+    
+    d5 += 1;
+    d5 %=10;
+    d4 += 1;
+    d4 %=10;
+    d3 += 1;
+    d3 %=10;
+    d2 += 1;
+    d2 %=10;
+    d1 += 1;
+    d1 %=10;
+    
+    result= (d1)*10000 + (d2)*1000 + (d3)*100 + (d4)*10 + d5;
     printf("\nNew num after adding 1 is = %d\n",result);
     return 0;
 }
