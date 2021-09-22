cb2ec92aa13b8869a477df056f6722381ccbad37
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-09-22 09:26:02 +0530

    Create Factorial.c

---
+++ Loop/Factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int fac=1,num,i;
+
+    printf("Enter the number you want factorial:-");
+    scanf("%d",&num);
+
+    for(i=1; i<num ;i++)
+    {
+        fac=fac*i;
+
+    }
+    printf("Factorial of %d is %d\t",num,fac);
+
+    return 0;
+}
