5f531f81c317e7c8b03f195d95c18a2443ab0117
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-09-23 17:10:04 +0530

    Update Prime or not.c

--- Loop/Prime or not.c
+++ Loop/Prime or not.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int n, i, flag = 0;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+
+  for (i = 2; i <= sqrt(n); ++i) {
+    // condition for non-prime
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  if (flag == 0) {
+    printf("%d is a Prime Number.");
+  } 
+ 
+    else
+      printf("%d is not a prime number.", n);
+  }
 
