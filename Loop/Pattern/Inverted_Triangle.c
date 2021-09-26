78c6cfb026d3acfad7166c4b72adccc695f0c316
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-09-27 00:36:41 +0530

    Update Inverted_Triangle.c

--- Loop/Pattern/Inverted_Triangle.c
+++ Loop/Pattern/Inverted_Triangle.c
@@ -15,3 +15,26 @@ int main()
 
     return 0;
 }
+
+//Inverted Triangle of numbers
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,rows;
+    printf("Enter the no. of Rows:-");
+    scanf("%d",&rows);
+    printf("\n\n");
+    
+    for(i=rows;i>=0;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf(" %d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
