73aad1003a88f6b45249e182c1ae66c39cc2d678
Abbassadri786 <90386455+Abbassadri786@users.noreply.github.com>
2021-10-06 19:50:36 +0530

    Create Descending_Order.c

---
+++ Arrays/Descending_Order.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+
+{
+   int i,j,n,count=0,temp=0;
+
+   scanf("%d",&n);
+
+   int arr[n];
+
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&arr[i]);
+   }
+
+for(i=0;i<n;i++)
+   {
+       for(j=0;j<n;j++)
+       {
+           if(arr[j]<arr[i] )
+           {
+            temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+           }
+           
+       }
+   }
+       
+       for(i=0;i<n;i++)
+       printf(" %d ",arr[i]);
+     
+   
+   return 0;
+
+}
