54cc6ae28f2d392db99cae446fa9c40d0dfc0478
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2021-11-16 13:44:10 +0530

    Create Sum_Of_Sub_Array.c

---
+++ Sum_Of_Sub_Array.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int num,i,j,sum=0;
+    long long int size;
+    scanf("%lld%d",&size,&num);
+    long long int arr[size];
+    
+    for(i=0;i<size;i++)
+    {
+        scanf("%lld",&arr[i]);
+    }
+    long long int n=size/arr[0];
+    
+    for (i = 0; i < n; i++) 
+    {
+        sum += arr[i];
+
+        for (j = i + 1; j <= n; j++) 
+        {
+            if (sum == num) 
+            {
+                printf( "%d %d\n",i+1, j);
+                break;
+                
+            }
+            else if (sum > num || j == n)
+                break;
+            sum = sum + arr[j];
+            
+        }
+    }
+    
+    
+    return 0;
+}
