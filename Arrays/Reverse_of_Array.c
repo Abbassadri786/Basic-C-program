bfc5af7c546ba81235c4782e4d0ea700bc7744e1
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-02-03 22:45:28 +0530

    Update Reverse_of_Array.c

--- Arrays/Reverse_of_Array.c
+++ Arrays/Reverse_of_Array.c
@@ -3,19 +3,25 @@
 
 int main()
 {
-    int size,i;
+    int size,i,temp;
     scanf("%d",&size);
     
-    int arr[size],rev[size];
-    for(i=0;i<size;i++)
+    int arr[size];
+    for(i=0; i<size; i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=size-1;i>=0;i--)
+    for(i=0; i<size/2; i++)
     {
-        rev[i]=arr[i];
-        printf("%d ",rev[i]);
+        temp=arr[i];
+        arr[i]=arr[size-i-1];
+        arr[size-i-1]=temp;
     }
+    for(i=0; i<size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
     
     return 0;
 }
