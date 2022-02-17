1d4ab11dc1358bef7f1d8f8d0ea7a6c8f3183f25
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-02-17 22:30:28 +0530

    Create Sliding_Window.cpp

---
+++ Special Algo/Sliding_Window.cpp
@@ -0,0 +1,11 @@
+// Find minimum sum subarray of given size k
+// maximum
+// sum
+ for(int i --- <k)
+   sum += arr[i];
+ int j = 0;
+   for(i = k; i<n)
+   sum -= arr[j]
+   j++;
+   sum += arr[i];
+   if(sum > maximu)
