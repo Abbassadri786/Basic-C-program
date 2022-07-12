a9afb9cdd4a1ad7d013d4475a2b312e3b5fc4865
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-07-12 17:38:45 +0530

    Update Leap_Year.c

--- If Else/Leap_Year.c
+++ If Else/Leap_Year.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter the year btw 1900 to 3000:-");
     scanf("%d",&x);
 
-    if(x%4==0 && x%100!=0 || x%400==0)
+    if((x%4==0 && x%100!=0) || x%400==0)
     {
         printf("\nYear is Leap Year\n");
     }
