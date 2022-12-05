32b2216fdd2be8789448314236f05b479b905d63
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-12-05 16:14:04 +0530

    Update Singly_Linked.cpp

--- Linked List/Singly_Linked.cpp
+++ Linked List/Singly_Linked.cpp
@@ -9,8 +9,17 @@ class Node{
     Node(int data){
         this -> data = data;
         this -> next = NULL;
+    }//Destructor
+    ~Node(){
+        int value = this -> data;
+        if(this->next != NULL){
+            delete next;
+           this -> next = NULL;
+        }
+        cout<< "Memory is free for node with data:"<<value<<endl;
     }
 };
+
 void InsertAtHead(Node* &head, int d){
     //new node create
     Node* temp = new Node(d);
@@ -30,16 +39,67 @@ void InserAtTail(Node* &tail, int d){
     tail -> next = temp;
     tail = temp;
 }
-void InsertAtMiddle()
+void InsertAtPosition(Node* &head, Node* &tail, int position, int d){
+    //Insert At Head
+    if(position==1){
+        InsertAtHead(head,d);
+        return;
+    }
+    Node* temp = head;
+    int cnt = 1;
+    while(cnt<position-1){
+        temp = temp->next;
+        cnt++;
+    }
+    Node* nodeToInsert = new Node(d);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+    if(temp->next == NULL){
+        InserAtTail(tail,d);
+        return;
+    } 
+}
+void deleteNode(int position, Node* &head){
+    //deleting start of node
+    if(position==1){
+        Node* temp = head;
+        head = head -> next;
+        //memory free start node 
+        temp -> next = NULL;
+        delete temp;
+    }
+    else{
+        Node* curr = head;
+        Node* prev = NULL;
+        int cnt=1;
+        
+        while(cnt < position){
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+        prev -> next = curr -> next;
+        curr -> next = NULL;
+        delete curr;
+    }
+}
+
 int main() {
     Node* node1 = new Node(10);
     Node* head = node1;
     Node* tail = node1;
     // Write C++ code here
     InsertAtHead(head, 30);
+    
     InserAtTail(tail,20);
     printList(head);
-       
+    
+    InsertAtPosition(head,tail,1,22);
+    printList(head);
+    
+    deleteNode(2,head);
+    printList(head);
+    
 
     return 0;
 }
