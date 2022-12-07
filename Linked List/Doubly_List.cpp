21c5cd92fe4ea9d03b216a427daaf1f7ec6eb5d1
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-12-07 22:26:06 +0530

    Update Doubly_List.cpp

--- Linked List/Doubly_List.cpp
+++ Linked List/Doubly_List.cpp
@@ -25,14 +25,53 @@ void print(Node* head){
     cout<<endl<<"Length of List:"<<len<<endl;
 }
 void insertAtHead(Node* &head, int d){
-    Node* temp = head;
+    Node* temp = new Node(d);
     temp -> next = head;
     head -> prev = temp;
+    head = temp;
+}
+void insertAtTail(Node* &tail,int d){
+    Node *temp = new Node(d);
+    tail->next = temp;
+    temp->prev = tail;
+    tail = temp;
+}
+void insertAtPosition(Node* &head,Node* &tail,int position, int d){
+    if(position==1){
+        insertAtHead(head,d);
+        return;
+    }
+    int cnt=0;
+    Node* temp = head;
+    while(cnt<position-1){
+        temp = temp->next;
+        cnt++;
+    }
+    if(temp->next == NULL){
+        insertAtTail(tail,d);
+        return;
+    }
+    Node* nodeToInsert= new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next->prev = nodeToInsert;
+    temp->next = nodeToInsert;
+    nodeToInsert->prev = temp;
+    
 }
 int main() {
     Node* node1= new Node(10);
-    node1 = head;
-
+    Node* head = node1;
+    Node* tail = node1;
+    
+    insertAtHead(head,20);
+    
+    insertAtHead(head,30);
+    
+    insertAtTail(tail, 40);
+    print(head);
+    
+    insertAtPosition(head,tail,6,25);
+    print(head);
 
     return 0;
 }
