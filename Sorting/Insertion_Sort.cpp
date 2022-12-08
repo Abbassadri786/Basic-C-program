5df324d572b9e6f4b2853b435544fb4470ed2dd8
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-12-08 14:37:20 +0530

    Create Insertion_Sort.cpp

---
+++ Sorting/Insertion_Sort.cpp
@@ -0,0 +1,12 @@
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i=1; i<n; i++){
+        int j= i-1;
+        int x = arr[i];
+        while(j>-1 && arr[j]>x){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = x;
+    }
+}
