2150763d3352479e4fe2d2d58814f2549c6cbff8
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2023-05-30 16:53:50 +0530

    Create Fabonacci Using Queues.cpp

---
+++ Queue/Fabonacci Using Queues.cpp
@@ -0,0 +1,17 @@
+// Fabonacci Series Using Queues
+
+#include<iostream>
+using namespace std;
+
+void fun(int n){
+    IntQueue q = new IntQueue();
+    q.enqueue(0);
+    q.enqueue(1);
+    for(int i=0; i<n; i++){
+        int a = q.dequeue();
+        int b = q.dequeue();
+        q.enqueue(b);
+        q.enqueue(a+b);
+        print(a);
+    }
+}
