8e52905d9e9f9a5bdf7a702dd226495cc9620054
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2023-06-12 19:58:26 +0530

    Update Detect_Loop.cpp

--- Linked List/Linked List Questions/Detect_Loop.cpp
+++ Linked List/Linked List Questions/Detect_Loop.cpp
@@ -2,20 +2,20 @@ class Solution
 {
     public:
     //Function to check if the linked list has a loop.
-    bool detectLoop(Node* head)
-    {
-        Node* High = head;
-        Node* Low = head;
+    bool detectLoop(Node* head){
+        Node* slow = head;
+        Node* fast = head;
         
-        while(High != NULL && High->next != NULL){
-            Low = Low->next;
-            High = High->next->next;
-            if(High==Low){
+        while(fast != NULL){
+            fast = fast -> next;
+            if(fast != NULL){
+                fast = fast -> next;
+                slow = slow -> next;
+            }
+            if(fast == slow){
                 return true;
             }
         }
         return false;
-        
-        
     }
 };
